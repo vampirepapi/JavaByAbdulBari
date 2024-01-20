5c7077b5a00796f47a2ffeb075ee43b33da51037
vampirepapi <shubhamsourabh8@gmail.com>
2024-01-20 15:53:25 +0530

    test

--- src/oops/inheritnace/ParametrisedConstructor.java
+++ src/oops/inheritnace/ParametrisedConstructor.java
@@ -1,5 +1,9 @@
 package oops.inheritnace;
 
 public class ParametrisedConstructor {
-    
+    private int value;
+
+    public ParametrisedConstructor(int value) {
+        this.value = value;
+    }
 }
